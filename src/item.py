ddf2ca534b0e9e5c102f08e57c69647e35057224
Steve Smythe <steve@smythefamily.com>
2022-10-16 11:31:35 -0700

    add dict and json string parsing

--- src/item.py
+++ src/item.py
@@ -1,3 +1,6 @@
+import json
+import math
+
 class Item:
     """
     Item is for store bought products
@@ -9,8 +12,11 @@ class Item:
     def set_name(self, name):
         self.name = name
 
-    def set_brand_name(self, brand_name):
-        self.brand_name = brand_name
+    def set_store_product_name(self, store_product_name):
+        self.store_product_name = store_product_name
+
+    def set_store_name(self, store_name):
+        self.store_name = store_name
 
     def set_price(self, price):
         self.price = price
@@ -20,9 +26,6 @@ class Item:
 
     def set_serving_size(self, serving_size):
         self.serving_size = serving_size
-
-    def set_kcal_per_serving(self, kcal_per_serving):
-        self.kcal_per_serving = kcal_per_serving
 
     def set_carb_per_serving(self, carb_per_serving):
         self.carb_per_serving = carb_per_serving
@@ -38,3 +41,36 @@ class Item:
 
     def cost_per_serving(self):
         return round(self.price / self.servings_per_container, 2)
+
+    def kcal_per_serving(self):
+        kcal_carb = self.carb_per_serving * 4
+        kcal_fat = self.fat_per_serving * 9
+        kcal_protein = self.protein_per_serving * 4
+        return math.ceil(kcal_carb + kcal_fat + kcal_protein)
+
+    def set_item_from_dict(self, data):
+        self.set_name(data['name'])
+        self.set_store_product_name(data['store_product_name'])
+        self.set_price(data['price'])
+        self.set_servings_per_container(data['servings_per_container'])
+        self.set_serving_size(data['serving_size'])
+        self.set_carb_per_serving(data['carb_per_serving'])
+        self.set_fat_per_serving(data['fat_per_serving'])
+        self.set_protein_per_serving(data['protein_per_serving'])
+        self.set_sodium_per_serving(data['sodium_per_serving'])
+
+    def set_item_from_json(self, json_str):
+        json_dict = json.loads(json_str)
+        data_dict = {}
+        name = list(json_dict.keys())[0]
+        data_dict['name'] = name
+        data_dict['price'] = json_dict[name]['price']
+        data_dict['store'] = json_dict[name]['store']
+        data_dict['store_product_name'] = json_dict[name]['store product name']
+        data_dict['servings_per_container'] = json_dict[name]['servings per container']
+        data_dict['serving_size'] = json_dict[name]['serving size']
+        data_dict['carb_per_serving'] = json_dict[name]['carb']
+        data_dict['fat_per_serving'] = json_dict[name]['fat']
+        data_dict['protein_per_serving'] = json_dict[name]['protein']
+        data_dict['sodium_per_serving'] = json_dict[name]['sodium']
+        self.set_item_from_dict(data_dict)
--- tests/test_item.py
+++ tests/test_item.py
@@ -3,6 +3,37 @@ from src.item import Item
 import pytest
 
 
+json_ezekiel_bread = '''
+{
+  "ezekiel bread": {
+    "store": "Safeway",
+    "store product name": "Food For Life Ezekiel 4:9 Organic Bread Sprouted Whole Grain - 24 Oz",
+    "serving size": "1 slice (34g)",
+    "servings per container": 20,
+    "price": 7.79,
+    "carb": 15,
+    "fat": 0.5,
+    "protein": 5,
+    "sodium": 75
+  }
+}
+'''
+
+
+dict_ezekiel_bread = {
+    'name': 'ezekiel bread',
+    'store_name': 'Safeway',
+    'store_product_name': 'Food For Life Ezekiel 4:9 Organic Bread Sprouted Whole Grain - 24 Oz',
+    'price': 7.79,
+    'servings_per_container': 20,
+    'serving_size': '1 slice (34g)',
+    'carb_per_serving': 15,
+    'fat_per_serving': 0.5,
+    'protein_per_serving': 5,
+    'sodium_per_serving': 75
+}
+
+
 @pytest.fixture
 def item():
     return Item()
@@ -13,15 +44,21 @@ def test_constructor(item):
 
 
 def test_set_name(item):
-    name = "ezekiel bread"
+    name = 'ezekiel bread'
     item.set_name(name)
     assert item.name == name
 
 
-def test_set_brand_name(item):
-    brand_name = "Food For Life Ezekiel 4:9 Organic Bread Sprouted Whole Grain - 24 Oz"
-    item.set_brand_name(brand_name)
-    assert item.brand_name == brand_name
+def test_set_store_name(item):
+    store_name = 'Safeway'
+    item.set_store_name(store_name)
+    assert item.store_name == store_name
+
+
+def test_set_store_product_name(item):
+    store_product_name = 'Food For Life Ezekiel 4:9 Organic Bread Sprouted Whole Grain - 24 Oz'
+    item.set_store_product_name(store_product_name)
+    assert item.store_product_name == store_product_name
 
 
 def test_set_price(item):
@@ -37,19 +74,13 @@ def test_set_servings_per_container(item):
 
 
 def test_set_serving_size(item):
-    serving_size = "1 slice (34g)"
+    serving_size = '1 slice (34g)'
     item.set_serving_size(serving_size)
     assert item.serving_size == serving_size
 
 
-def test_set_kcal_per_serving(item):
-    kcal_per_serving = 80
-    item.set_kcal_per_serving(kcal_per_serving)
-    assert item.kcal_per_serving == kcal_per_serving
-
-
 def test_set_carb_per_serving(item):
-    carb_per_serving = 0.5
+    carb_per_serving = 15
     item.set_carb_per_serving(carb_per_serving)
     assert item.carb_per_serving == carb_per_serving
 
@@ -61,22 +92,61 @@ def test_set_fat_per_serving(item):
 
 
 def test_set_protein_per_serving(item):
-    protein_per_serving = 0.5
+    protein_per_serving = 5
     item.set_protein_per_serving(protein_per_serving)
     assert item.protein_per_serving == protein_per_serving
 
 
+def test_kcal_per_serving(item):
+    carb_per_serving = 15
+    item.set_carb_per_serving(carb_per_serving)
+    fat_per_serving = 0.5
+    item.set_fat_per_serving(fat_per_serving)
+    protein_per_serving = 5
+    item.set_protein_per_serving(protein_per_serving)
+    assert item.kcal_per_serving() == 85
+
+
 def test_set_sodium_per_serving(item):
-    sodium_per_serving = 0.5
+    sodium_per_serving = 75
     item.set_sodium_per_serving(sodium_per_serving)
     assert item.sodium_per_serving == sodium_per_serving
 
 
 def test_cost_per_serving(item):
-    name = "ezekiel bread"
+    name = 'ezekiel bread'
     servings_per_container = 20
     price = 7.79
     item.set_name(name)
     item.set_price(price)
     item.set_servings_per_container(servings_per_container)
     assert item.cost_per_serving() == 0.39
+
+
+def test_set_item_from_dict(item):
+    item.set_item_from_dict(dict_ezekiel_bread)
+    assert item.name == 'ezekiel bread'
+    assert item.store_product_name == 'Food For Life Ezekiel 4:9 Organic Bread Sprouted Whole Grain - 24 Oz'
+    assert item.price == 7.79
+    assert item.servings_per_container == 20
+    assert item.serving_size == '1 slice (34g)'
+    assert item.carb_per_serving == 15
+    assert item.fat_per_serving == 0.5
+    assert item.protein_per_serving == 5
+    assert item.sodium_per_serving == 75
+    assert item.kcal_per_serving() == 85
+
+
+def test_set_item_from_json(item):
+    item.set_item_from_json(json_ezekiel_bread)
+    assert item.name == 'ezekiel bread'
+    assert item.store_product_name == 'Food For Life Ezekiel 4:9 Organic Bread Sprouted Whole Grain - 24 Oz'
+    assert item.price == 7.79
+    assert item.servings_per_container == 20
+    assert item.serving_size == '1 slice (34g)'
+    assert item.carb_per_serving == 15
+    assert item.fat_per_serving == 0.5
+    assert item.protein_per_serving == 5
+    assert item.sodium_per_serving == 75
+    assert item.cost_per_serving() == 0.39
+    assert item.kcal_per_serving() == 85
