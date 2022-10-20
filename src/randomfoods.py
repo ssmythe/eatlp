d29ac3fd0204b1f78340ffa5a415a4168ddc27f3
Steve Smythe <steve@smythefamily.com>
2022-10-20 08:49:54 -0700

    fix random.sample deprecation message

--- src/randomfoods.py
+++ src/randomfoods.py
@@ -3,6 +3,7 @@ from src.food import Food
 
 import random
 
+
 class RandomFoods:
     """
     RandomFoods is used to contain multiple Food objects with calculated
@@ -24,6 +25,7 @@ class RandomFoods:
                 self.dict_of_random_foods[food] = foods.dict_of_foods[food]
         if set_random_seed != None:
             random.seed(set_random_seed)
-        list_of_randomly_selected_foods = random.sample(foods.dict_of_foods.keys(), num_of_random_foods)
+        list_of_randomly_selected_foods = random.sample(
+            tuple(foods.dict_of_foods.keys()), num_of_random_foods)
         for food in list_of_randomly_selected_foods:
             self.dict_of_random_foods[food] = foods.dict_of_foods[food]
