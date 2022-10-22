d0a59874d1060626c43f462609acffa41a8ccb97
Steve Smythe <steve@smythefamily.com>
2022-10-21 20:14:40 -0700

    add weight loss schedule

---
+++ weight_loss_schedule.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+
+# TODO add to user.json: start weight date, start weight lbs, target bmi, activity level, loss per week lbs
+# TODO use MSJ forumla to determine target kcal, target weight loss date
+
