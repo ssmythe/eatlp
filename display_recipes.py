52aa5a36a2efb9ed2e81299e42c84992e5bc162a
Steve Smythe <steve@smythefamily.com>
2022-11-09 09:42:34 -0800

    improve output of display recipes

--- display_recipes.py
+++ display_recipes.py
@@ -16,7 +16,9 @@ foods = Foods()
 foods.read_foods_from_json_file('data/foods.json')
 
 for recipe_key, recipe in recipes.dict_of_recipes.items():
-    print (f"Recipe: {recipe_key}")
+    servings_per_recipe = recipes.dict_of_recipes[recipe_key].dict_of_recipe['servings_per_recipe']
+    serving_size = recipes.dict_of_recipes[recipe_key].dict_of_recipe['serving_size']
+    print (f"Recipe: {recipe_key} (makes {servings_per_recipe} servings of {serving_size})")
     print('-' * 114)
 
     food = foods.dict_of_foods[recipe_key]
@@ -28,6 +30,7 @@ for recipe_key, recipe in recipes.dict_of_recipes.items():
     sodium = food['sodium_per_serving']
     price = food['price_per_serving']
     priceperkcal = food['price_per_kcal_per_serving'] * 100
+    serving_size = food['serving_size']
     cent_symbol = chr(162)
     print("%-30s kcal %4d, carb %4d, fat %3d, protein %3d, sodium %4d, fiber %4d, $%6.2f, %2.2f%s" %
         (recipe_key, kcal, carb, fat, protein, sodium, fiber, price, priceperkcal, cent_symbol))
